f885038e1c34a6c37c6899375d8022b508f08265
chavda-bhavik <bhavikvchavda@gmail.com>
2022-11-26 10:45:17 +0530

    feat: Added Dal library

---
+++ libs/dal/src/repositories/base.repository.ts
@@ -0,0 +1,45 @@
+export interface IDataObject {
+  id: string;
+}
+
+export class BaseRepository<T extends IDataObject> {
+  public data: T[];
+
+  constructor() {
+    this.data = [];
+  }
+
+  create(entity: T) {
+    this.data.push(entity);
+  }
+
+  getAll() {
+    return this.data;
+  }
+
+  getById(id: string) {
+    return this.data.find((entity) => entity.id === id);
+  }
+
+  update(id: string, entity: T) {
+    const index = this.data.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.data[index] = entity;
+      return entity;
+    }
+    return null;
+  }
+
+  delete(id: string) {
+    const index = this.data.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.data.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  deleteAll() {
+    this.data = [];
+  }
+}
---
+++ libs/dal/src/repositories/todo/todo.repository.ts
@@ -0,0 +1,4 @@
+import { BaseRepository } from '../base.repository';
+import { TodoEntity } from '@monorepo/shared';
+
+export class TodoRepository extends BaseRepository<TodoEntity> {}
